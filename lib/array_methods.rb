b5ae5a75593ee766d2cb9e0de97d703d90d906ed
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-24 22:31:58 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,22 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.length.times do |count|
+    if array[count] == value_to_find
+      return count
+    	end
+    end
+  nil
 end
 
 def find_max_value(array)
-  # Add your solution here
+   array.length.times do |index|
+  puts array[index]
+  
+  counter = 1 
+  if array[index] < counter
+    += counter
+  else
+    return counter
+end
 end
 
 def find_min_value(array)

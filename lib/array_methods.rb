106a8e4fef4fb6446a6bfa7b1622e184b222dab6
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-24 22:51:15 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -31,5 +31,11 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  # Add your solution here
+ placeholder = 0
+  array.length.times do |index|
+    if array[index] < placeholder
+      placeholder = array[index]
+   end
+  end
+  return placeholder
 end

444f69e9f010e1dec6f85aed636ee62f3729f22a
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-24 22:50:13 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,15 +11,24 @@ end
    #array.length.times do |index|
   #puts array[index]
   
-  counter = 1 
-  if array[index] < counter
-    += counter
-  else
-    return counter
-end
-end
+  #counter = 1 
+ # if array[index] < counter
+   # += counter
+  #else
+   # return counter
+#end
+#end
 
-def find_max_value
+
+def find_max_value(array)
+  placeholder = 0
+  array.length.times do |index|
+    if array[index] > placeholder
+      placeholder = array[index]
+   end
+  end
+  return placeholder
+end
 
 def find_min_value(array)
   # Add your solution here
